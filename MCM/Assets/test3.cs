e762d16f209ab2148ecf046c8d58f4ae0178e16c
c3l1l <celil.ozturk@windowslive.com>
2020-12-24 23:31:41 +0300

    test3
    test3

--- MCM/Assets/test3.cs
+++ MCM/Assets/test3.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test2 : MonoBehaviour
+public class test3 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
